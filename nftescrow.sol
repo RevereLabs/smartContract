2bf73d06b80d236fb0c28356b0832dc10bcf774b
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2022-08-16 16:56:17 +0530

    .

--- nftescrow.sol
+++ nftescrow.sol
@@ -16,9 +16,15 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+contract RGCNFTInterface {
+    function mint(
+        address _to,
+        string memory tokenURI_
+    ) external;
+}
 contract nftescrow is IERC721Receiver {
     
-    enum ProjectState {newEscrow, nftDeposited, receivedFundsByClient, receivedFundsByFreelancer, checkpointingStarted, checkpointsDone, distributionDone, cancelledByClient, cancelledByFreelancer, delivered}
+    enum ProjectState {newEscrow, nftDeposited, receivedFundsByClient, checkpointingStarted, checkpointsDone, done, cancelledByClient, cancelledByFreelancer,}
     event Received(address, uint);
 
     event ClientCheckpointChanged(uint8);
@@ -26,15 +32,19 @@ contract nftescrow is IERC721Receiver {
 
     event FundsDisbursed(uint8 _from, uint8 _to);
 
+    event ProjectStateChanged(uint _state);
+
 // TODO: why public?
 // TODO: token address can change ,rn hardcoded
 
-    address public tokenAddress = '';
+    address public RTNAddress = '';
 
     address payable public clientAddress;
     address payable public freelancerAddress;
-    address public nftAddress;
-    uint256 tokenID;
+    address public RNFTAddress;
+    address public RGCNFTAddress;
+    
+    uint256 RNFTTokenID;
 
     uint clientAmount;
     uint freelancerStake;
@@ -57,19 +67,22 @@ contract nftescrow is IERC721Receiver {
     // NOTE: You NEED to use cumulative percentages.
     uint8[] checkpoints;
 
+    string RGCNFTArtURI;
+
     // IERC20 public token; // Address of token contract
     // address public transferOperator; // Address to manage the Transfers
 
     constructor(uint8[] memory _checkpoints, string _freelancer, uint _clientAmount , uint _freelancerStake ) payable
     {
-    
         require(_checkpoints[_checkpoints.length-1]==100);
         clientAddress = payable(msg.sender);
         freelancerAddress = payable(_freelancer);
         checkpoints = _checkpoints;
         clientAmount = _clientAmount;
         freelancerStake = _freelancerStake;
-        projectState = ProjectState.newEscrow;
+        setProjectState(ProjectState.newEscrow);
+
+        RGCNFTArtURI = '';
     }
 
    	modifier condition(bool _condition) {
@@ -86,15 +99,10 @@ contract nftescrow is IERC721Receiver {
 		require(msg.sender == freelancerAddress);
 		_;
 	}
-	
+
 	modifier noDispute(){
 	    require(buyerCancel == false && sellerCancel == false);
 	    _;
-	}
-	
-	modifier BuyerOrSeller() {
-		require(msg.sender == buyerAddress || msg.sender == sellerAddress);
-		_;
 	}
 	
 	modifier inProjectState(ProjectState _state) {
@@ -102,30 +110,119 @@ contract nftescrow is IERC721Receiver {
 		_;
 	}
 
-     function depositNFT(address _NFTAddress, uint256 _TokenID)
+    function setRGCNFTURI(string memory _RGCNFTArtURI) public {
+        RGCNFTArtURI = _RGCNFTArtURI;
+    }
+
+    function setProjectState(ProjectState _state) private {
+        projectState = _state;
+        ProjectStateChanged(projectState);
+    }
+
+    function getProjectState() public view returns (ProjectState) {
+        return projectState;
+    }
+
+
+    // Project Lifecycle
+
+     function depositRNFT(address _RNFTAddress, uint256 _RNFTTokenID)
         public
         inProjectState(ProjectState.newEscrow)
         onlyClient
     {
-        nftAddress = _NFTAddress;
-        tokenID = _TokenID;
-        ERC721(nftAddress).safeTransferFrom(msg.sender, address(this), tokenID);
-        projectState = ProjectState.nftDeposited;
+        RNFTAddress = _RNFTAddress;
+        RNFTTokenID = _RNFTTokenID;
+        ERC721(RNFTAddress).safeTransferFrom(msg.sender, address(this), RNFTTokenID);
+        setProjectState(ProjectState.nftDeposited);
     } 
 
     
-    function depositAsClient()
+    // The transfer should have been approved by the client.
+    function depositFundsAsClient()
         public
         inProjectState(ProjectState.nftDeposited)
         onlyClient
-        // payable
-        // inProjectState(ProjectState.nftDeposited)
     {
-        // buyerAddress = payable(msg.sender);
-        IERC20(tokenAddress).transferFrom(msg.sender, address(this), clientAmount);
-        projectState = ProjectState.receivedFundsByClient;
+        IERC20(RTNAddress).transferFrom(msg.sender, address(this), clientAmount);
+        setProjectState(ProjectState.receivedFundsByClient);
+    }
 
-    }  
+    /// @dev The transfer should be approved by freelancer
+    function depositFundsAsFreelancer()
+        public
+        inProjectState(ProjectState.receivedFundsByClient)
+        onlyFreelancer
+    {
+        IERC20(RTNAddress).transferFrom(msg.sender, address(this), freelancerStake);
+        setProjectState(ProjectState.checkpointingStarted);
+    }
+
+    function increaseFreelancerCheckpoint() 
+    external 
+    inProjectState(ProjectState.checkpointingStarted) 
+    onlyFreelancer
+    {
+        require(currentCheckpoints.freelancer < (checkpoints.length - 1));
+        currentCheckpoints.freelancer++;
+        emit FreelancerCheckpointChanged(currentCheckpoints.freelancer);
+    }
+
+    function increaseClientCheckpoint() 
+    external 
+    inProjectState(ProjectState.checkpointingStarted) 
+    onlyClient
+    {
+        require(currentCheckpoints.client < (checkpoints.length - 1));
+        currentCheckpoints.client++;
+        emit ClientCheckpointChanged(currentCheckpoints.client);    
+    }    
+
+    function disburseFunds() 
+    public 
+    onlyFreelancer
+    {
+        uint8 approvedCheckpoint = currentCheckpoints.client;
+        if (currentCheckpoints.freelancer < currentCheckpoints.client) {
+            approvedCheckpoint = currentCheckpoints.freelancer;
+        }
+        if (currentCheckpoints.smc < approvedCheckpoint) {
+            uint8 percentageToBeTransferred = checkpoints[approvedCheckpoint] - checkpoints[currentCheckpoints.smc];
+            // Transfer percentageToBeTransferred*totalAmount to freelancer
+            IERC20(RTNAddress).transferFrom(address(this), freelancerAddress, percentageToBeTransferred*clientAmount/100);
+            emit FundsDisbursed(currentCheckpoints.smc, approvedCheckpoint);
+            currentCheckpoints.smc = approvedCheckpoint;
+            if (approvedCheckpoint == checkpoints.length - 1) {
+                checkpointingDone();
+            }
+        }
+    }
+
+    function checkpointingDone()
+        private
+        inProjectState(ProjectState.checkpointingStarted)
+    {
+        setProjectState(ProjectState.checkpointsDone);
+        releaseStakedFunds();
+        releaseGigNFTToClient();
+        mintGigCompletionNFTForFreelancer();
+        setProjectState(ProjectState.done);
+    }
+
+    function releaseStakedFunds()
+    private
+    inProjectState(ProjectState.checkpointsDone)
+    {
+        IERC20(RTNAddress).transferFrom(address(this), freelancerAddress, freelancerStake);
+    }
+
+    function releaseGigNFTToClient() {
+        ERC721(RNFTAddress).transferFrom(address(this), clientAddress, tokenID);
+    }
+
+    function mintGigCompletionNFTForFreelancer() private {
+        RGCNFTInterface(RGCNFTAddress).mint(freelancerAddress, RGCNFTArtURI);
+    }
 
 
 
@@ -139,36 +236,13 @@ contract nftescrow is IERC721Receiver {
         return checkpoints;
     }
 
-    function increaseFreelancerCheckpoint() external onlySeller {
-        require(currentCheckpoints.freelancer < (checkpoints.length - 1));
-        currentCheckpoints.freelancer++;
-        emit FreelancerCheckpointChanged(currentCheckpoints.freelancer);
-    }
-
-    function setClientCheckpoint() external onlyBuyer {
-        require(currentCheckpoints.client < (checkpoints.length - 1));
-        currentCheckpoints.client++;
-        emit ClientCheckpointChanged(currentCheckpoints.client);    
-    }    
-
-    function disburseFunds() public BuyerOrSeller {
-        uint8 approvedCheckpoint = currentCheckpoints.client;
-        if (currentCheckpoints.freelancer < currentCheckpoints.client) {
-            approvedCheckpoint = currentCheckpoints.freelancer;
-        }
-        if (currentCheckpoints.smc < approvedCheckpoint) {
-            uint8 percentageToBeTransferred = checkpoints[approvedCheckpoint] - checkpoints[currentCheckpoints.smc];
-            // TODO: Transfer percentageToBeTransferred*totalAmount to freelancer
-            emit FundsDisbursed(currentCheckpoints.smc, approvedCheckpoint);
-            currentCheckpoints.smc = approvedCheckpoint;
-        }
-    }
 
 
-    function balanceofERC20(address TokenAddress) public view returns ( uint256 ){
-        return IERC20(TokenAddress).balanceOf(address(this));
-    }
-    
+
+
+
+    // copied code from here
+
     function onERC721Received( address operator, address from, uint256 tokenId, bytes calldata data ) public override returns (bytes4) {
         return this.onERC721Received.selector;
     }
